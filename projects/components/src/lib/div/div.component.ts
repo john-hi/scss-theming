8dc3f63fff810604a94cda3cf9cf820a7b936b59
John Hi <john-hi@yandex.ru>
2020-02-19 19:54:23 +0300

    feat(themes): add themes by material approach

--- projects/components/src/lib/div/div.component.ts
+++ projects/components/src/lib/div/div.component.ts
@@ -1,10 +1,12 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'lib-div',
   templateUrl: './div.component.html',
   styleUrls: ['./div.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  // tslint:disable-next-line: use-component-view-encapsulation
+  encapsulation: ViewEncapsulation.None
 })
 export class DivComponent implements OnInit {
 
